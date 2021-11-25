7642707250e75057f44af9220572cf8d425cf1f5
Prasoon Shukla <prasoonshukla9002@gmail.com>
2021-11-25 13:27:11 +0000

    Deleted controllers/admin.controller.js, controllers/course.controller.js, controllers/fee.controller.js, controllers/school.controller.js, controllers/student.controller.js, middlewares/index.js, models/Admin.js, models/Course.js, models/Fee.js, models/School.js, models/Student.js, routes/AdminRoutes.js, routes/CourseRoutes.js, routes/FeeRoutes.js, routes/SchoolRoutes.js, routes/StudentRoutes.js, services/adminService.js, services/courseService.js, services/feeService.js, services/schoolService.js, services/studentService.js, .env, app.js, db.js, package-lock.json, package.json

--- routes/AdminRoutes.js
+++
@@ -1,8 +0,0 @@
-const express =  require("express");
-const router = express.Router();
-const adminController = require('../controllers/admin.controller');
-
-router.put('/updateAdmin/:adminId', adminController.apiUpdateAdmin);
-router.delete('/deleteAdmin/:adminId', adminController.apiDeleteAdmin);
-
-module.exports = router;
--- routes/FeeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express =  require("express");
-const router = express.Router();
-const feeController = require('../controllers/fee.controller');
-
-router.post('/addFees/:courseId', feeController.apiAddFees);
-router.put('/updatedFees/:feeId', feeController.apiUpdateFees);
-router.delete('/deleteFees/:courseId/:feeId', feeController.apiDeleteFees);
-
-module.exports = router;
